848342f2a2a113fed93e8f59913105cd69a4fc39
Stilllee <orodae@gmail.com>
2024-01-23 00:09:48 +0900

    [this 바인딩] 화살표 함수를 이벤트리스너로 사용

--- src/field.js
+++ src/field.js
@@ -10,8 +10,7 @@ export default class Field {
     this.bugCount = bugCount;
     this.field = document.querySelector(".game__field");
     this.fieldRect = this.field.getBoundingClientRect();
-    this.onClick = this.onClick.bind(this);
-    this.field.addEventListener("click", this.onClick);
+    this.field.addEventListener("click", (event) => this.onClick(event));
   }
 
   init() {
