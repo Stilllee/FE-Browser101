1c9b8f7aade5354707d212c484dfc45e789477a3
Stilllee <orodae@gmail.com>
2024-01-23 23:39:01 +0900

    함수를 깔끔하게 정리하기

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 "use strict";
 import PopUp from "./popup.js";
+import * as sound from "./sound.js";
 import { Reason, GameBuilder } from "./game.js";
 
 const gameFinishBanner = new PopUp();
@@ -15,12 +16,15 @@ game.setGameStopListener((reason) => {
   switch (reason) {
     case Reason.cancel:
       message = "REPLAY❓";
+      sound.playAlert();
       break;
     case Reason.win:
       message = "YOU WON 🎉";
+      sound.playWin();
       break;
     case Reason.lose:
       message = "YOU LOST 💩";
+      sound.playBug();
       break;
     default:
       throw new Error("not valid reason");
